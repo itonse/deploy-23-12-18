d69a1c7215bae4438da36fe15be83af31183d15b
itonse <govl3dnjs@gmail.com>
2023-12-19 14:39:47 +0900

    work

--- src/main/java/com/example/deploy231218/Deploy231218Application.java
+++ src/main/java/com/example/deploy231218/Deploy231218Application.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Deploy231218Application {
 
     public static void main(String[] args) {
+        System.out.println("안녕하세요~~~");
         SpringApplication.run(Deploy231218Application.class, args);
     }
 
