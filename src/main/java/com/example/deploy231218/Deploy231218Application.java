1bbb115b0d03bdd69a1ed34028c421d0d270fb1b
itonse <govl3dnjs@gmail.com>
2023-12-19 14:33:03 +0900

    Initial commit

---
+++ src/main/java/com/example/deploy231218/Deploy231218Application.java
@@ -0,0 +1,13 @@
+package com.example.deploy231218;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Deploy231218Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Deploy231218Application.class, args);
+    }
+
+}
